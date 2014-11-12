0cd9360239bea31eef6d52143869a3457019a0ab
Sam Zagrobelny <strangesast@gmail.com>
2014-11-12 17:18:06 -0500

    modified mongodb location

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ var config = {};
 
 config.name = "Scoreboard2";
 
-config.mongoUrl = "mongodb://127.0.0.1:27017/allData";
+config.mongoUrl = "mongodb://zagrobelny.us:27017/allData";
 
 config.validTypes = ['region', 'game', 'player'];
 
